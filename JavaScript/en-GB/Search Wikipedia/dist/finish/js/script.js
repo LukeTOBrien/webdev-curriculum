5dfd5845b2bf3e1dd3868f0ac796ef0fdef72b78
Luke <obrienluk89@gmail.com>
2017-06-23 17:04:27 +0100

    New lessons

---
+++ JavaScript/en-GB/Search Wikipedia/dist/finish/js/script.js
@@ -0,0 +1,55 @@
+function displayAnimals() {
+    var container = $("#animal-container");
+    for (var index = 0; index < animals.length; index++) {
+        var animal = animals[index];
+        var div = $('<div/>');
+
+        div.on("click", { animal : animal }, function(event) {
+            var animal = event.data.animal;
+            var isSelected = animal.isSelected;
+            animal.isSelected = ! isSelected;
+            $(this).toggleClass('selected');
+        });
+
+        var img = $("<img/>");
+        img[0].src = animals[index].img;
+
+        div.append(img);
+
+        container.append(div);
+    }
+}
+
+function getSelectedAnimal() {
+    return animals.find(function(animal) {
+        return animal.isSelected
+    });
+}
+
+function getAllSelectedAnimals() {
+    return animals.filter(function(animal) {
+        return animal.isSelected
+    });
+}
+
+/*
+ * Display Animal's favourite video'
+ */
+function displaySelectedAnimalFavouriteVideo(animal) {
+    var output = $("#output");
+
+    var video = $("<video />");
+    video[0].controls = true;
+    var src = $("<source />")
+    src[0].src = animal.favouriteVideo;
+    video.append(src);
+    video[0].load();
+    
+    output.append(video);
+}
+
+function displayFavouriteVideo() {
+    $("#output").empty();
+    var selectedAnimal = getAllSelectedAnimals();
+    selectedAnimal.forEach(displaySelectedAnimalFavouriteVideo);
+}
